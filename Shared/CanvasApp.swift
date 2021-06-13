116e98e066ef2630445fa082c206366a0b2b6170
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-13 22:11:51 +0200

    Initial Commit

---
+++ Shared/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
